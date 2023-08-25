861961f7060dcc8066e9cb2dacd9f890ca976a6f
Alfred <alfred_radar@yahoo.fr>
2023-08-25 10:52:31 -0500

    changed file

--- jenkinsscrip.sh
+++ jenkinsscrip.sh
@@ -15,7 +15,8 @@ systemctl start docker
 usermod -aG docker ubuntu
 
 ## Extra packages install
-apt install vim git -y
+apt-get install vim git -y
+apt-get install wget git -y
 
 # run jenkins
 mkdir -p /var/jenkins_home
